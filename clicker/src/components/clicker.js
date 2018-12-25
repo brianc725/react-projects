de111690943df7e432d015c11fd9e9adadb11d73
brianc725 <brianc725@gmail.com>
2018-12-24 23:54:03 -0800

    Implement functionality of clicker, need to style

--- clicker/src/components/clicker.js
+++ clicker/src/components/clicker.js
@@ -2,13 +2,39 @@ import React, { Component } from 'react';
 import { Button, ButtonGroup } from 'reactstrap';
 
 class Clicker extends Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            value: 0,
+        }
+    }
+
+    addOne = () => {
+        this.setState((state) => ({
+            value: state.value + 1,
+        }));
+    }
+
+    reset = () => {
+        this.setState((state) => ({
+            value: 0,
+        }));
+    }
+
+    subtractOne = () => {
+        this.setState((state) => ({
+            value: state.value - 1,
+        }))
+    }
+
+
   render() {
     return (
         <div style={style_container}>
-            <h1>Clicker</h1>
-            <Button color="success">+</Button>
-            <Button color="warning">reset</Button>
-            <Button color="danger">-</Button>
+            <h1>{this.state.value}</h1>
+            <Button outline color="success" onClick={this.addOne}>+</Button>
+            <Button outline color="warning" onClick={this.reset}>reset</Button>
+            <Button outline color="danger" onClick={this.subtractOne}>-</Button>
         </div>
     );
   }
@@ -16,7 +42,11 @@ class Clicker extends Component {
 
 const style_container = {
     align: 'center',
-    color: 'red',
+    color: 'white',
+    background: 'black',
+    width: '50%',
+    height: '50%',
+    padding: '30',
 }
 
 export default Clicker;
